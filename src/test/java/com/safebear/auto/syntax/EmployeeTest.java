763f71a2270961be0fa095102a311ce698e0e7ff
student <student@safebear.co.uk>
2018-12-17 15:30:41 +0000

    Created employee tests for syntax examples

---
+++ src/test/java/com/safebear/auto/syntax/EmployeeTest.java
@@ -0,0 +1,35 @@
+package com.safebear.auto.syntax;
+
+import org.testng.annotations.Test;
+
+public class EmployeeTest {
+
+    @Test
+    public void testEmployee() {
+
+
+
+    Employee hannah = new Employee();
+    Employee bob = new Employee();
+    SalesEmployee victoria = new SalesEmployee();
+
+
+    //Employee Hannah and fire Bob
+    hannah.employ();
+    bob.fire();
+    victoria.employ();
+    victoria.changeCar("bmw");
+
+
+    //Print out employment status to screen
+        System.out.println("Hannahs employment state: " + hannah.employed);
+        System.out.println("Bobss employment state: " + bob.employed);
+        System.out.println("Victoria employment state: "+victoria.employed);
+        System.out.println("Victoria's car: " + victoria.car);
+
+
+
+
+} //test
+
+}//class
