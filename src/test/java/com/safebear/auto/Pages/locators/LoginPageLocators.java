2401ae1a73cc7c77181e5a4d97a2f09b19945e88
student <student@safebear.co.uk>
2018-12-18 16:04:27 +0000

    Added selectors for success and error messages

--- src/test/java/com/safebear/auto/Pages/locators/LoginPageLocators.java
+++ src/test/java/com/safebear/auto/Pages/locators/LoginPageLocators.java
@@ -1,4 +1,4 @@
-package locators;
+package com.safebear.auto.Pages.locators;
 
 import lombok.Data;
 import org.openqa.selenium.By;
@@ -8,6 +8,7 @@ public class LoginPageLocators {
     private By usernameLocator = By.id("username");
     private By passwordLocator = By.id("password");
     private By loginBttonLoctaor = By.id ("enter");
+    private By loginNotSuccessfulMessage = By.xpath("//p[@id='rejectLogin']");
 
 
 }
