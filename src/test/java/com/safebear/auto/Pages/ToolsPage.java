b16932c2802c9fa78d29126a6635af0a5ef342f7
student <student@safebear.co.uk>
2018-12-18 13:37:10 +0000

    Updated Lombok annotations because I had wrong ones

---
+++ src/test/java/com/safebear/auto/Pages/ToolsPage.java
@@ -0,0 +1,23 @@
+package com.safebear.auto.Pages;
+
+import com.safebear.auto.Pages.locators.ToolsPageLocators;
+import com.sun.istack.internal.NotNull;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.openqa.selenium.WebDriver;
+
+@RequiredArgsConstructor
+public class ToolsPage {
+
+    ToolsPageLocators locator = new ToolsPageLocators();
+
+    @NonNull WebDriver driver;
+
+
+
+
+
+
+
+}
