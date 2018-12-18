e39813a8cbf47f2b7930d115f6436dd74c54397a
student <student@safebear.co.uk>
2018-12-18 10:01:12 +0000

    Created employee tests for syntax examples

---
+++ src/test/java/com/safebear/auto/tests/LoginTests.java
@@ -0,0 +1,38 @@
+package com.safebear.auto.tests;
+
+import org.testng.annotations.Test;
+import utils.Utils;
+
+public class LoginTests extends BaseTest{
+
+
+
+    @Test
+    public void validLogin(){
+        driver.get(Utils.getUrl());
+
+    }
+
+
+    @Test
+    public void invalidLogin(){
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}//class
---
+++ src/test/java/utils/Utils.java
@@ -0,0 +1,44 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Utils {
+
+    private static final String URL = System.getProperty("url", "http://localhost:8080");
+    private static final String BROWSER = System.getProperty("browser", "chrome");
+
+
+
+    public static String getUrl(){
+        return URL;
+    }
+
+    public static WebDriver getDriver(){
+        System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\drivers\\chromedriver.exe");
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("window-size=1366,768");
+
+        switch (BROWSER){
+            case "chrome":
+                return new ChromeDriver();
+            case "firefox":
+                return new FirefoxDriver();
+            default:
+                return new ChromeDriver();
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+}
