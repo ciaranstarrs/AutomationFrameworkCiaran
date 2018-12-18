02c4dd59a53a89c68820dc51f7a547183b1fecee
student <student@safebear.co.uk>
2018-12-18 13:33:13 +0000

    Added methods to LoginPage.java

---
+++ src/test/java/com/safebear/auto/Pages/LoginPage.java
@@ -0,0 +1,36 @@
+package com.safebear.auto.Pages;
+
+import com.safebear.auto.Pages.locators.LoginPageLocators;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.openqa.selenium.WebDriver;
+
+@RequiredArgsConstructor
+public class LoginPage {
+
+    LoginPageLocators locators = new LoginPageLocators();
+
+    @NonNull WebDriver driver;
+
+    public String getPageTitle(){
+
+
+        return driver.getTitle().toString();
+    }//getpageTitle
+
+    public void enterUsername(String uname){
+        driver.findElement(locators.getUsernameLocator()).sendKeys(uname);
+
+    }//enterUsername
+
+    public void enterPassword(String pword){
+        driver.findElement(locators.getPasswordLocator()).sendKeys(pword);
+    }
+
+    public void clickLoginButton(){
+        driver.findElement(locators.getLoginBttonLoctaor()).click();
+    }
+
+}
+
+
